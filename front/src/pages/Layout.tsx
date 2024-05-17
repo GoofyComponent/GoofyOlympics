7760b9e7aa696b541a28d186f0d37acd848538b2
Antoine Azevedo Da Silva <azevedo-a@hotmail.fr>
2024-05-17 18:25:49 +0200

    feat(front): add paris2024 to header
    #6

--- front/src/pages/Layout.tsx
+++ front/src/pages/Layout.tsx
@@ -2,8 +2,15 @@ import { Link, Outlet } from '@tanstack/react-router';
 import { User } from 'lucide-react';
 
 import olympics_logo from '@/assets/goofyolympics_logo.svg';
+import paris_logo from '@/assets/logoparis2024.svg';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 export const Layout = () => {
   return (
@@ -27,16 +34,40 @@ const Header = () => {
             GoofyOlympics
           </h1>
         </div>
-        <div className="my-auto space-x-4 ml-6">
-          <Link to="/" className="[&.active]:text-black text-black/60">
+        <div className="my-auto flex justify-start space-x-4 ml-6">
+          <Link
+            to="/"
+            className="[&.active]:text-black text-black/60 my-auto hover:text-black/75 transition-all"
+          >
             Summary
           </Link>
-          <Link to="/athletes" className="[&.active]:text-black text-black/60">
+          <Link
+            to="/athletes"
+            className="[&.active]:text-black text-black/60 my-auto hover:text-black/75 transition-all"
+          >
             Athletes
           </Link>
-          <Link to="/countries" className="[&.active]:text-black text-black/60">
+          <Link
+            to="/countries"
+            className="[&.active]:text-black text-black/60 my-auto hover:text-black/75 transition-all"
+          >
             Countries
           </Link>
+          <TooltipProvider delayDuration={1}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  to="/2024"
+                  className="[&.active]:text-black text-black/60 my-auto hover:opacity-75 transition-all"
+                >
+                  <img src={paris_logo} title="PARIS 2024" className="w-10 mx-auto" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>PARIS 2024 Page</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
       {/* TODO: Solution temporaire - changer par un burger */}
---
+++ front/src/routes/_mainapp/2024.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/_mainapp/2024')({
+  component: () => <div>Hello /_mainapp/2024!</div>,
+});
